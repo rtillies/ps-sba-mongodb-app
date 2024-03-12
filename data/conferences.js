799b6837c5c233ab089a82978cd7b7a74934a99d
Richard T <74796181+rtillies@users.noreply.github.com>
2024-03-12 09:08:52 -0400

    csv converted to json

---
+++ data/conferences.js
@@ -0,0 +1,14 @@
+const conferences = [
+  {
+    "ID": 1,
+    "Conference": "South",
+    "Description": "States below the Mason-Dixon line"
+  },
+  {
+    "ID": 2,
+    "Conference": "North",
+    "Description": "U.S. states north of Washington DC"
+  }
+]
+
+module.exports = conferences;
---
+++ data/teams.js
@@ -0,0 +1,92 @@
+const teams = [
+  {
+    "ID": 1,
+    "Team Name": "VA Ballerz",
+    "Conference": "South",
+    "Captain": "Professor",
+    "Teammate": "Omar",
+    "City": "Roanoke",
+    "State": "VA",
+    "Wins": 6,
+    "Loss": 0
+  },
+  {
+    "ID": 2,
+    "Team Name": "JQ Suited",
+    "Conference": "South",
+    "Captain": "Janet",
+    "Teammate": "Quin",
+    "City": "Raleigh",
+    "State": "NC",
+    "Wins": 2,
+    "Loss": 4
+  },
+  {
+    "ID": 3,
+    "Team Name": "Star Studded",
+    "Conference": "South",
+    "Captain": "Reece",
+    "Teammate": "Dee",
+    "City": "Dallas",
+    "State": "TX",
+    "Wins": 1,
+    "Loss": 5
+  },
+  {
+    "ID": 4,
+    "Team Name": "That Couple",
+    "Conference": "South",
+    "Captain": "That Dude",
+    "Teammate": "That Chick",
+    "City": "St. Louis",
+    "State": "MO",
+    "Wins": 3,
+    "Loss": 3
+  },
+  {
+    "ID": 5,
+    "Team Name": "Gerow Animals",
+    "Conference": "North",
+    "Captain": "Chad",
+    "Teammate": "Junbug",
+    "City": "Harlem",
+    "State": "NY",
+    "Wins": 0,
+    "Loss": 6
+  },
+  {
+    "ID": 6,
+    "Team Name": "Team Bui",
+    "Conference": "North",
+    "Captain": "Daniel",
+    "Teammate": "Tiffani",
+    "City": "Washington",
+    "State": "DC",
+    "Wins": 5,
+    "Loss": 1
+  },
+  {
+    "ID": 7,
+    "Team Name": "Jerzeys Finest",
+    "Conference": "North",
+    "Captain": "Marcus",
+    "Teammate": "Sniper",
+    "City": "Teaneck",
+    "State": "NJ",
+    "Wins": 3,
+    "Loss": 3
+  },
+  {
+    "ID": 8,
+    "Team Name": "Knuck If You Stuck",
+    "Conference": "North",
+    "Captain": "Aceito",
+    "Teammate": "Deborah",
+    "City": "Hartford",
+    "State": "CT",
+    "Wins": 4,
+    "Loss": 2
+  }
+]
+
+module.exports = teams;
