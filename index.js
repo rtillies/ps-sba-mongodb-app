e8350cf32b56a5510f045d8523ba2ea2592fb172
Richard T <74796181+rtillies@users.noreply.github.com>
2024-03-31 13:27:47 -0400

    connect to port and db

--- index.js
+++ index.js
@@ -10,59 +10,32 @@ const dotenv = require('dotenv')
 
 const app = express();
 const port = 3000;
-dotenv.config();
+// dotenv.config();
 
-// Require routes from routes folder
-const teamRoutes = require('./routes/teamRoutes')
-const gameRoutes = require('./routes/gameRoutes')
-const confRoutes = require('./routes/confRoutes')
+// // Require routes from routes folder
+// const teamRoutes = require('./routes/teamRoutes')
+// const gameRoutes = require('./routes/gameRoutes')
+// const confRoutes = require('./routes/confRoutes')
 
 // Environment variables
 dotenv.config();
 
-/* DATABASE CONNECTION */
-
-/* MIDDLEWARE */
-
-// BodyParser
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json({extended: true}))
-
-// serve static files from the css directory
-app.use('/css', express.static('css'));
-
-// Use Router Middleware - 
-app.use('/api/teams/', teamRoutes)
-app.use('/api/games/', gameRoutes)
-app.use('/api/conferences/', confRoutes)
-
-// specify the views directory
-// register the template engine
-app.set("views", "./views"); 
-app.set("view engine", "ejs")
-// app.set("view engine", "pug")
+function main() {
+  portDB();
 
 
-// Error handling
-app.use((err, req, res, next) => {
-  res.status(400).send(err.message)
-})
+}
 
-/* ROOT ROUTES - no router needed */
-
-// GET: render index for root route
-app.get('/', (req, res) => {
-  // res.send("Root GET Route works")
-  // app.use(express.static("public"))
-  res.render('index')
-})
-
-// GET: use regex for all routes log*
-app.get('/log*', (req, res) => {
-  res.render('login')
-})
+function portDB() {
+  app.listen(port, () => {
+    console.log(`The server is listening on port ${port}`);
+  })
+  
+  mongoose.connect(process.env.ATLAS_URI)
+    .then(console.log("Connected!"))
+    .catch(e => console.error(e.message));
+}
 
 // Listen to the port
-app.listen(port, () => {
-  console.log(`The server is listening on port ${port}`);
-})
+
+main();
