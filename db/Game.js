ee9f42a4d398fa4d30439bd2f22bda06e749b53c
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-01 11:48:45 -0400

    Add index on date-winner combo

--- db/Game.js
+++ db/Game.js
@@ -40,34 +40,43 @@ const gameSchema = new mongoose.Schema({
     type: Date,
     default: () => Date.now(),
   },
-})
+});
 
-gameSchema.statics.findByTeam = function(team) {
+// Teams can only play one game per day
+// so combination of date and winner is unique
+gameSchema.index(
+  {
+    date: 1,
+    winner: 1,
+  },
+  { unique: true }
+);
+
+gameSchema.statics.findByTeam = function (team) {
   return this.find({
     $or: [
-      {homeTeam: new RegExp(team, 'i')},
-      {awayTeam: new RegExp(team, 'i')},
-    ]
-  })
-}
+      { homeTeam: new RegExp(team, "i") },
+      { awayTeam: new RegExp(team, "i") },
+    ],
+  });
+};
 
-gameSchema.query.byTeam = function(team) {
+gameSchema.query.byTeam = function (team) {
   return this.where({
     $or: [
-      {homeTeam: new RegExp(team, 'i')},
-      {awayTeam: new RegExp(team, 'i')},
-    ]
-  })
+      { homeTeam: new RegExp(team, "i") },
+      { awayTeam: new RegExp(team, "i") },
+    ],
+  });
+};
 
-}
+gameSchema.pre("save", function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
 
-gameSchema.pre('save', function(next) {
-  this.updatedAt = Date.now()
-  next()
-}) 
-
-gameSchema.methods.updateGame = function() {
+gameSchema.methods.updateGame = function () {
   console.log(`Game ${this._id} updated.`);
-}
+};
 
 module.exports = mongoose.model("Game", gameSchema);
